8a9ec49bba1106bb89eba230fc137ef0c9a5857e
schnee98 <jooeun06161@gmail.com>
2024-06-30 04:41:40 +0900

    chore: export 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       "/": { page: "/" },
