bd319ba4d028366c5769b44b500a8d9224715efc
schnee98 <jooeun06161@gmail.com>
2024-06-30 05:07:22 +0900

    chore: 정적 prefix 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const prefix = process.env.NODE_ENV === "production" ? "https://schnee98.github.io/" : "";
+
 const nextConfig = {
   output: "export",
+  assetPrefix: prefix,
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       "/": { page: "/" },
