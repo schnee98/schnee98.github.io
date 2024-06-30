407e2caf09dbb45c4c53b5f3e441fd9de5b90a71
schnee98 <jooeun06161@gmail.com>
2024-06-30 13:04:29 +0900

    chore: 배포 디버깅 코드 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const prefix = process.env.NODE_ENV === "production" ? "https://schnee98.github.io/" : "";
 
 const nextConfig = {
   output: "export",
-  assetPrefix: prefix,
 };
 
 export default nextConfig;
