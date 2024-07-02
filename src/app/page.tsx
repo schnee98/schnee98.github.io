62cb7b55e769846d4ee460db206f946300d0fd6c
schnee98 <jooeun06161@gmail.com>
2024-07-02 12:52:39 +0900

    feat: 포스트 구분 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from "@/styles/page.module.css";
 import Navigation from "@/components/navigation/Navigation";
 import PostDescription from "@/components/post/PostDescription";
 import { getPosts } from "@/utils/postUtils";
+import HorizontalRule from "@/components/post/HorizontalRule";
 
 const Home = async () => {
   const posts = await getPosts();
@@ -14,8 +15,12 @@ const Home = async () => {
         <h2 className={styles.description}>주니어 프론트엔드 개발자</h2>
       </header>
       <main className={styles.main}>
+        <HorizontalRule />
         {posts.map((post, index) => (
-          <PostDescription key={`blog-post-${index}`} {...post} />
+          <div key={`blog-post-${index}`}>
+            <PostDescription {...post} />
+            <HorizontalRule />
+          </div>
         ))}
       </main>
     </>
