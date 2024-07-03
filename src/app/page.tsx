a566ea8cdc409ab7072ff4d4b1e1648e135e5425
schnee98 <jooeun06161@gmail.com>
2024-07-03 20:33:28 +0900

    feat: 이메일, Github 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import PostDescription from "@/components/post/PostDescription";
 import { getPosts } from "@/utils/postUtils";
 import HorizontalRule from "@/components/post/HorizontalRule";
 
+const EMAIL = "jooeun06161@gmail.com";
+const GITHUB_URL = "github.com/schnee98";
+
 const Home = async () => {
   const posts = await getPosts();
   return (
@@ -13,6 +16,12 @@ const Home = async () => {
         <div className={styles.background}></div>
         <h1>박주은, Schnee</h1>
         <h2 className={styles.description}>주니어 프론트엔드 개발자</h2>
+        <div>
+          📧 <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+        </div>
+        <div>
+          😺 <a href={GITHUB_URL}>{GITHUB_URL}</a>
+        </div>
       </header>
       <main className={styles.main}>
         <HorizontalRule />
