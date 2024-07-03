73f1f29b36ef165b134d4ba693989032f20d7732
schnee98 <jooeun06161@gmail.com>
2024-07-03 15:38:10 +0900

    refactor: 블로그 사이드바 클릭 이벤트 및 내비게이션 메뉴 스타일링 변경

--- src/components/navigation/ContentSidebar.tsx
+++ src/components/navigation/ContentSidebar.tsx
@@ -2,14 +2,17 @@ import styles from "@/styles/navigation.module.css";
 
 interface ContentSidebarProps {
   headers: Element[];
+  handleClick: () => void;
 }
 
-export default function ContentSidebar({ headers }: ContentSidebarProps) {
+export default function ContentSidebar({ headers, handleClick }: ContentSidebarProps) {
   return (
     <>
-      <div className={styles.sidebarBlur}></div>
+      <div className={styles.sidebarBlur} onClick={handleClick}></div>
       <div className={styles.sidebar}>
-        <button className={styles.sidebarCloseButton}>X</button>
+        <button className={styles.sidebarCloseButton} onClick={handleClick}>
+          X
+        </button>
         <h3 className={styles.sidebarTitle}>목차</h3>
         <hr className={styles.sidebarBreak} />
         {headers.map(({ id, textContent }, index) => (
