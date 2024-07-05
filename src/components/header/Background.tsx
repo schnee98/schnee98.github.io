ecd052e36f5c1fdbb85ad65168cf8ae4360cb200
schnee98 <jooeun06161@gmail.com>
2024-07-05 15:15:40 +0900

    refactor: 애니메이션 함수 분리

--- src/components/header/Background.tsx
+++ src/components/header/Background.tsx
@@ -1,25 +1,31 @@
 "use client";
 
-import { RefObject, useEffect, useRef, useState } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import styles from "@/styles/page.module.css";
+
+const ANIMATION_DURATION = 7000;
+
+const renderWord = (content: string) =>
+  `<span class=${styles.swiftUpSpan}><i class=${styles.swiftUpIcon}>${content}</i></span>`;
+
+const renderNode = (node: Element, index: number) => node.setAttribute("style", `animation-delay: ${index * 0.2}s;`);
+
+const renderSwiftUpElement = (element: Element, ref: RefObject<HTMLDivElement>) => {
+  const words = element?.textContent?.split(" ") || [];
+
+  element.innerHTML = "";
+
+  words.forEach((_, index) => (words[index] = renderWord(words[index])));
+
+  element.innerHTML = words.join(" ");
+
+  const children = ref.current?.querySelectorAll("span > i");
+  children?.forEach(renderNode);
+};
 
 const startAnimation = (ref: RefObject<HTMLDivElement>) => {
   const swiftUpElements = ref.current?.querySelectorAll(".swift-up-text");
-  swiftUpElements?.forEach((elem) => {
-    const words = elem?.textContent?.split(" ") || [];
-
-    elem.innerHTML = "";
-
-    words.forEach(
-      (el, index) =>
-        (words[index] = `<span class=${styles.swiftUpSpan}><i class=${styles.swiftUpIcon}>${words[index]}</i></span>`)
-    );
-
-    elem.innerHTML = words.join(" ");
-
-    const children = ref.current?.querySelectorAll("span > i");
-    children?.forEach((node, index) => node.setAttribute("style", `animation-delay: ${index * 0.2}s;`));
-  });
+  swiftUpElements?.forEach((elem) => renderSwiftUpElement(elem, ref));
 };
 
 const Background = () => {
@@ -27,7 +33,7 @@ const Background = () => {
 
   useEffect(() => {
     startAnimation(textRef);
-    const interval = setInterval(() => startAnimation(textRef), 5000);
+    const interval = setInterval(() => startAnimation(textRef), ANIMATION_DURATION);
 
     return () => clearInterval(interval);
   }, []);
@@ -36,7 +42,7 @@ const Background = () => {
     <div ref={textRef} className={styles.animatedText}>
       <div className={styles.background}></div>
       <h1 className={`swift-up-text ${styles.swiftUpH1}`}>Welcome To </h1>
-      <h1 className={`swift-up-text ${styles.swiftUpH1}`}>Schnee&rsquo;s Blog</h1>
+      <h1 className={`swift-up-text ${styles.swiftUpH1}`}>Schnee&rsquo;s Blog!</h1>
     </div>
   );
 };
