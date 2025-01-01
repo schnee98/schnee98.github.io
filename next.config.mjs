67e9e781e4b71026461188f5523042a37009ef35
schnee98 <jooeun06161@gmail.com>
2025-01-02 05:10:03 +0900

    chore: svg 아이콘 컴포넌트 추가 및 적용 방식 수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,32 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
