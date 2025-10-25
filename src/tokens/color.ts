4410c6fa6c3d3b9c421c7a15b3727741e03f6556
schnee98 <jooeun06161@gmail.com>
2025-10-26 04:22:48 +0900

    feat: color 추가

---
+++ src/tokens/color.ts
@@ -0,0 +1,25 @@
+export const colors = {
+  accent: {
+    primary: "var(--color-accent-primary)",
+    secondary: "var(--color-accent-secondary)",
+    tertiary: "var(--color-accent-tertiary)",
+  },
+  text: {
+    primary: "var(--color-text-primary)",
+    secondary: "var(--color-text-secondary)",
+  },
+  natural: {
+    gray: {
+      100: "var(--color-natural-gray-100)",
+      200: "var(--color-natural-gray-200)",
+      300: "var(--color-natural-gray-300)",
+    },
+    white: "var(--color-natural-white)",
+  },
+  background: {
+    primary: "var(--color-background-primary)",
+  },
+  border: {
+    primary: "var(--color-border-primary)",
+  },
+};
